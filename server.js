abbbaeb9e254e0bf6a464bc52d54cf5c24f20f38
Louise Nyambati <louisen1993@gmail.com>
2017-04-22 11:25:54 -0500

    added routes

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ var mysql = require("mysql");
 var express = require("express");
 var url = require("url");
 var http = require("http");
+var path = require("path");
 
 var app = express();
 var PORT = 3000;
@@ -19,6 +20,22 @@ connection.connect(function(err) {
   console.log("It's connected!");
 });
 var server = http.createServer();
+
 server.listen(PORT, function(){
   console.log("Server is listening on: http://localhost:%s" , PORT);
 });
+
+// function handleRequest(req,res) {
+
+// }
+  app.get("/", function (req,res){
+    res.sendFile(path.join(__dirname, index.html))
+  }
+
+  app.get("/reserve", function (req,res){
+    res.sendFile(path.join(__dirname, reserve.html))
+  }
+
+  app.get("/table", function (req,res){
+    res.sendFile(path.join(__dirname, table.html))
+  }
